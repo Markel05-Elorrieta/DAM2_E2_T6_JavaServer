21eb8d4dd15ad3dcef458630ef92deb0958ce555
Markel <markel.romango@elorrieta-errekamari.com>
2025-01-31 12:54:18 +0100

    ekaprus

--- src/controller/DataThread.java
+++ src/controller/DataThread.java
@@ -52,7 +52,7 @@ public class DataThread extends Thread {
 	        
 			logger = Logger.getLogger("DataThread");
 			logger.severe("DataThread created");
-			logger.setLevel(Level.INFO);
+			logger.setLevel(Level.SEVERE);
 			
 		} catch (IOException e) {
 			logger.severe("Error creating DataThread" + e.getMessage());
